e018c728471b732034f3bb73354cad283fade7db
michelbeyrouty <michelbeyrouty@gmail.com>
2021-11-12 01:59:16 +0200

    master

--- app.js
+++ app.js
@@ -3,50 +3,58 @@ const fs = require('fs');
 const PNG = require('pngjs').PNG;
 const pixelmatch = require('pixelmatch');
 
-// Convert PDF to image
-const options = {
-  density: 100,
-  saveFilename: 'converted',
-  savePath: './images',
-  format: 'png',
-  width: 600,
-  height: 600,
-};
-const filePath = './files/ok_sample.pdf';
-const pageToConvertAsImage = 1;
-const isBase64 = false;
+main();
 
 
-fromPath(filePath, options).convert(pageToConvertAsImage, isBase64);
 
-// storeAsImage(pageToConvertAsImage).then(() => {
 
-//   storeAsImage(pageToConvertAsImage).then(() => {
+async function main(){
+  const options = {
+    density: 100,
+    saveFilename: 'convertedOk',
+    savePath: './images',
+    format: 'png',
+    width: 600,
+    height: 600,
+  };
+  const okSamplefilePath = './files/ok_sample.pdf';
+  const blankFilePath = './files/blank.pdf';
+  const pageToConvertAsImage = 1;
+  const isBase64 = false;
 
-//     // Calculate pixel difference
+  console.log('Converting PDF to image -> ' + okSamplefilePath);
+  await fromPath(okSamplefilePath, options)(pageToConvertAsImage, isBase64);
 
-//     const blankReferenceImage = PNG.sync.read(fs.readFileSync('./images/blank.png'));
-//     const convertedOkImage = PNG.sync.read(fs.readFileSync('./images/converted.1.png'));
+  console.log('Converting PDF to image -> ' + blankFilePath);
+  options.saveFilename = 'convertedBlank';
+  await fromPath(blankFilePath, options)(pageToConvertAsImage, isBase64);
 
-//     const { width, height } = blankReferenceImage;
+  // Calculate pixel difference
 
-//     const diff = new PNG( {
-//       width,
-//       height,
-//     } );
+  const blankReferenceImage = PNG.sync.read(fs.readFileSync('./images/blank.png'));
+  const convertedOkImage = PNG.sync.read(fs.readFileSync('./images/convertedOk.1.png'));
+  const convertedBlankImage = PNG.sync.read(fs.readFileSync('./images/convertedBlank.1.png'));
 
-//     const numDiffPixelsWithBlank = pixelmatch(blankReferenceImage.data, convertedOkImage.data, diff.data, width, height, {
-//       threshold: 0.1,
-//     });
+  const { width, height } = blankReferenceImage;
 
-//     const numDiffPixelsWithOk = pixelmatch(blankReferenceImage.data, img2.data, diff.data, width, height, {
-//       threshold: 0.1,
-//     });
+  const diff = new PNG( {
+    width,
+    height,
+  } );
 
-//     console.log('numDiffPixels with ok_sample:  ' + numDiffPixelsWithBlank);
-//     console.log('numDiffPixels with blank:  ' + numDiffPixelsWithOk);
+  const numDiffPixelsWithBlank = pixelmatch(blankReferenceImage.data, convertedBlankImage.data, diff.data, width, height, {
+    threshold: 0.1,
+  });
 
-//     return numDiffPixels;
+  const numDiffPixelsWithOk = pixelmatch(blankReferenceImage.data, convertedOkImage.data, diff.data, width, height, {
+    threshold: 0.1,
+  });
 
-//   });
-// });
+  console.log('numDiffPixels with ok_sample:  ' + numDiffPixelsWithBlank);
+  console.log('numDiffPixels with blank:  ' + numDiffPixelsWithOk);
+
+
+
+
+}
+
